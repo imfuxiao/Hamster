eac6c4c3167425f8cca693b42dfa59c8b0db217d
fu.xiao <im.fu.xiao@gmail.com>
2023-01-10 15:46:23 +0800

    Initial Commit

---
+++ Hamster/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Hamster
+//
+//  Created by morse on 10/1/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hamster/HamsterApp.swift
@@ -0,0 +1,17 @@
+//
+//  HamsterApp.swift
+//  Hamster
+//
+//  Created by morse on 10/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct HamsterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
