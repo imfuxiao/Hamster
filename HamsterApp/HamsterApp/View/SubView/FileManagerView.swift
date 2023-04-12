0308aa0a161cbc0bf71808db35e99c63e622ff80
fu.xiao <im.fu.xiao@gmail.com>
2023-04-12 20:58:34 +0800

    fix: #9 上传功能IP不能正常显示

--- HamsterApp/HamsterApp/View/SubView/FileManagerView.swift
+++ HamsterApp/HamsterApp/View/SubView/FileManagerView.swift
@@ -80,31 +80,21 @@ struct FileManagerView: View {
         .onAppear {
           Logger.shared.log.debug("FileManagerView appear")
 
-          // 屏幕长亮, 防止wifi无法使用
+          // 保持屏幕长亮, 防止wifi无法使用
           UIApplication.shared.isIdleTimerDisabled = true
 
-          let localIPs = UIDevice.current.localIP()
-          if localIPs.count == 1 {
-            self.localIP = localIPs[0].1
+          if let localIP = UIDevice.current.localIP() {
+            self.localIP = localIP
           }
+          self.monitor = .init(requiredInterfaceType: .wifi)
           self.monitor.pathUpdateHandler = { path in
             if path.status == .satisfied {
               self.wifiEnable = true
-              for (name, ip) in localIPs {
-                let name = path.availableInterfaces
-                  .map { $0.name }
-                  .first { $0 == name }
-                if name != nil {
-                  self.localIP = ip
-                }
-              }
               return
             }
             self.wifiEnable = false
           }
-
-          let queue = DispatchQueue.global(qos: .background)
-          self.monitor.start(queue: queue)
+          self.monitor.start(queue: .main)
         }
         .onDisappear {
           Logger.shared.log.debug("FileManagerView disppear")
@@ -121,45 +111,42 @@ struct FileManagerView: View {
   }
 }
 
+/// 获取IP
+/// - 解决方案: https://stackoverflow.com/questions/30748480/swift-get-devices-wifi-ip-address
 extension UIDevice {
+  
   /**
    Returns device ip address. Nil if connected via celluar.
    */
-  func localIP() -> [(String, String)] {
-    var address: [(String, String)] = []
+  func localIP() -> String? {
+    var address: String?
     var ifaddr: UnsafeMutablePointer<ifaddrs>?
-
+    
     if getifaddrs(&ifaddr) == 0 {
       var ptr = ifaddr
       while ptr != nil {
         defer { ptr = ptr?.pointee.ifa_next } // memory has been renamed to pointee in swift 3 so changed memory to pointee
-
+        
         guard let interface = ptr?.pointee else {
-          return address
+          return nil
         }
         let addrFamily = interface.ifa_addr.pointee.sa_family
         if addrFamily == UInt8(AF_INET) || addrFamily == UInt8(AF_INET6) {
           guard let ifa_name = interface.ifa_name else {
-            return address
+            return nil
           }
           let name = String(cString: ifa_name)
-
-          if name.hasPrefix("en") { // String.fromCString() is deprecated in Swift 3. So use the following code inorder to get the exact IP Address.
+          
+          if name == "en0" { // String.fromCString() is deprecated in Swift 3. So use the following code inorder to get the exact IP Address.
             var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
-            getnameinfo(
-              interface.ifa_addr, socklen_t(interface.ifa_addr.pointee.sa_len), &hostname,
-              socklen_t(hostname.count), nil, socklen_t(0), NI_NUMERICHOST
-            )
-            let addr = String(cString: hostname)
-            if !addr.contains("en") {
-              address.append((name, addr))
-            }
+            getnameinfo(interface.ifa_addr, socklen_t(interface.ifa_addr.pointee.sa_len), &hostname, socklen_t(hostname.count), nil, socklen_t(0), NI_NUMERICHOST)
+            address = String(cString: hostname)
           }
         }
       }
       freeifaddrs(ifaddr)
     }
-
+    
     return address
   }
 }
