0d3cb3dee308f8920439755abdd41eaa9ab1f535
fu.xiao <im.fu.xiao@gmail.com>
2023-04-16 18:46:15 +0800

    resolved: #43 shell: Resources/SharedSupport is not a directory

--- InputSchemaBuild.sh
+++ InputSchemaBuild.sh
@@ -4,8 +4,14 @@ set -e
 
 # 输入方案
 OUTPUT=".HamsterInputSchemas"
+if [[ ! -d Resources/SharedSupport ]]
+then
+  mkdir -p Resources/SharedSupport
+else
+  rm -rf Resources/SharedSupport/*
+fi
 rm -rf $OUTPUT && (
     git clone --depth 1 https://github.com/imfuxiao/HamsterInputSchemas.git $OUTPUT
     cd $OUTPUT
     make
-) && rm -rf Resources/SharedSupport/* && cp -R $OUTPUT/.SharedSupport/* Resources/SharedSupport/
+) && cp -R $OUTPUT/.SharedSupport/* Resources/SharedSupport/
