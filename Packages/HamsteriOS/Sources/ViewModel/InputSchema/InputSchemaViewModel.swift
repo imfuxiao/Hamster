6d3f952b6bdb3b64b8401d8388128aa3fb691e48
fu.xiao <im.fu.xiao@gmail.com>
2023-09-28 23:47:21 +0800

    fix: 部署/导入/快捷指令中对部署后配置文件读取的异常处理

--- Packages/HamsteriOS/Sources/ViewModel/InputSchema/InputSchemaViewModel.swift
+++ Packages/HamsteriOS/Sources/ViewModel/InputSchema/InputSchemaViewModel.swift
@@ -78,16 +78,20 @@ extension InputSchemaViewModel {
       try await rimeContext.deployment(configuration: hamsterConfiguration)
 
       // 读取 Rime 目录下 hamster.yaml 配置文件，如果存在
-      let configuration = try HamsterConfigurationRepositories.shared.loadFromYAML(FileManager.hamsterConfigFileOnUserDataSupport)
-      hamsterConfiguration = configuration
+      if FileManager.default.fileExists(atPath: FileManager.hamsterConfigFileOnUserDataSupport.path) {
+        hamsterConfiguration = try HamsterConfigurationRepositories.shared.loadFromYAML(FileManager.hamsterConfigFileOnUserDataSupport)
+      }
 
-      // 读取 Rime 目录下 hamster.custom.yaml 配置文件(如果存在)，并对相异的配置做 merge 合并（已 hamster.custom.yaml 文件为主）
-      let patchConfiguration = try HamsterConfigurationRepositories.shared.loadPatchFromYAML(yamlPath: FileManager.hamsterPatchConfigFileOnUserDataSupport)
-      if let configuration = patchConfiguration.patch {
-        hamsterConfiguration = try hamsterConfiguration.merge(
-          with: configuration,
-          uniquingKeysWith: { _, patchValue in patchValue }
-        )
+      // 读取 Rime 目录下 hamster.custom.yaml 配置文件(如果存在)，
+      // 并对相异的配置做 merge 合并（已 hamster.custom.yaml 文件为主）
+      if FileManager.default.fileExists(atPath: FileManager.hamsterPatchConfigFileOnUserDataSupport.path) {
+        let patchConfiguration = try HamsterConfigurationRepositories.shared.loadPatchFromYAML(yamlPath: FileManager.hamsterPatchConfigFileOnUserDataSupport)
+        if let configuration = patchConfiguration.patch {
+          hamsterConfiguration = try hamsterConfiguration.merge(
+            with: configuration,
+            uniquingKeysWith: { _, patchValue in patchValue }
+          )
+        }
       }
 
       HamsterAppDependencyContainer.shared.configuration = hamsterConfiguration
