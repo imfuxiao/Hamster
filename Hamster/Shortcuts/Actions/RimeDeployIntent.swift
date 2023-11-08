b87e401227c87603efaafc7cee413ccb417f9dae
fu.xiao <im.fu.xiao@gmail.com>
2023-11-08 10:57:16 +0800

    chore: RIME 重新部署快捷指令优化

--- Hamster/Shortcuts/Actions/RimeDeployIntent.swift
+++ Hamster/Shortcuts/Actions/RimeDeployIntent.swift
@@ -10,29 +10,26 @@ import HamsteriOS
 import HamsterKeyboardKit
 import HamsterKit
 import OSLog
+import SwiftUI
 
 @available(iOS 16.0, *)
 struct RimeDeployIntent: AppIntent {
   static var title: LocalizedStringResource = "RIME 重新部署"
 
+  static var openAppWhenRun: Bool {
+    return true
+  }
+
+  static var authenticationPolicy: IntentAuthenticationPolicy {
+    .requiresAuthentication
+  }
+
   static var description = IntentDescription("仓输入法 - RIME 重新部署")
 
-  func perform() async throws -> some ReturnsValue & ProvidesDialog {
-    var hamsterConfiguration = HamsterAppDependencyContainer.shared.configuration
-    do {
-      if hamsterConfiguration.general?.enableAppleCloud ?? false {
-        // 先打开iCloud地址，防止Crash
-        _ = URL.iCloudDocumentURL
-      }
-
-      try HamsterAppDependencyContainer.shared.rimeContext.deployment(configuration: &hamsterConfiguration)
-
-      HamsterAppDependencyContainer.shared.configuration = hamsterConfiguration
-
-      return .result(dialog: .init("重新部署完成"))
-    } catch {
-      Logger.statistics.error("RimeDeployIntent failed: \(error)")
-      return .result(dialog: .init("重新部署失败:\(error.localizedDescription)"))
-    }
+  @MainActor
+  func perform() async throws -> some ReturnsValue {
+    HamsterAppDependencyContainer.shared.mainViewModel.navigationToRIME()
+    HamsterAppDependencyContainer.shared.mainViewModel.execShortcutCommand(.rimeDeploy)
+    return .result()
   }
 }
